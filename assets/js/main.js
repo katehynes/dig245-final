d0598a5e76b53de391f1d9553fff032f7862d14c
Kate Hynes <kahynes@davidson.edu>
2022-11-16 12:33:40 -0500

    adding images

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,21 +58,32 @@
 
 $(document).ready(function(){
     $('#sample-size').on('change', function(){
-      const PERCENT = 8.4;
-    	let numPeople = $(this).val();
-      let numWith = Math.floor((PERCENT * numPeople)/100);
+      console.log("check");
+      // const PERCENT = 8.4;
+    	let numPeople = Number($(this).val());
+      // let numWith = Math.floor((PERCENT * numPeople)/100);
       // want to show the 1st 25, 50, 100 images, etc.
       // thinking about adding classes to the images?
-      for (let i = 0; i < numWith; i++) {
-        if (i < numWith) {
-          // add/toggle class that displays these images with a blue border
-        }
-        else {
-          // add/toggle class that displays images with a black border for people "without"
-        }
+      // add seperate classes for the people in each sample who are "with" and "without"
+      // $("div.with-"numPeople).
+      let str = "";
+      for (let i = 0; i < numPeople; i++) {
+        str += `
+          <div class="col">
+            <img src="assets/img/users/${i}.jpg" class="img-fluid">
+          </div>
+        `;
+
+        // if (i < numWith) {
+        //   // add/toggle class that displays these images with a blue border
+        // }
+        // else {
+        //   // add/toggle class that displays images with a black border for people "without"
+        // }
         // show the first numPeople – each image would have a class saying if it's in the first 25, 50 people, etc.
-        $("div."+numPeople).show();
+        // $("div."+numPeople).show();
       }
+      $(".visualization").html(str);
     });
 });
 
