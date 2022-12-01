1f144641d676ca85593f3e821c478a0fa65d9598
Kate Hynes <kahynes@davidson.edu>
2022-11-30 21:48:31 -0500

    added all initial design details
    added image borders to visualization, added margins, removed dead code, added comments when needed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,65 @@
-
 /* javascript */
-// let data = [
-//   {
-//     path: 'assets/img/avatar-7a02db53e3e8a88d1e667d92388ee930.jpg',
-//   },
-//   {
-//
-//   },
-//   {
-//
-//   },
-//
-// ]
-//
-//
-// let genOption = 100, // number of people to display
-//
-//
-// /**
-//  * Return [count] number of random indexes from [arr]
-//  */
-// function returnRandomArrIndexes(arr, count) {
-//   // make sure count is not greater than number of options
-//   if (count > arr.length) count = arr.length;
-//   // to store chosen indexes
-//   let chosen = [];
-//   // loop until length of chosen arr matches number requested
-//   while (chosen.length < count) {
-//     var r = Math.floor(Math.random() * arr.length);
-//     if (chosen.indexOf(r) === -1) chosen.push(r);
-//   }
-//   return chosen;
-// }
-// function randomPhrase(count = 1) {
-//   let chosen = returnRandomArrIndexes(data, count);
-//   // console.log(chosen);
-//   // loop through chosen indexes, append to string
-//   let str = "";
-//   for (let i = 0; i < chosen.length; i++) {
-//     str += `<tr>
-//             <th scope="row">${(i+1)}</th>
-//             <td>${data[chosen[i]].path}</td>
-//             <td><a href="${data[chosen[i]].url}" target="_blank">${data[chosen[i]].title}</a></td>
-//             <td>${data[chosen[i]].publication}</td>
-//             <td>${data[chosen[i]].year}</td>
-//             </tr>`;
-//   }
-//   return str;
-// }
-//
-// function populateSelect() {
-//   let $select = $("#genOption");
-//   $.each(data, function (i) {
-//     $select.append($("<option />").val(i).text(i));
-//   });
-// }
-
-$(document).ready(function(){
-    $('#sample-size').on('change', function(){
-    	let numPeople = Number($(this).val());
-      let str = "";
-      for (let i = 0; i < numPeople; i++) {
+// On user selection, populate the div with the "visualization" class with images
+// downloaded from "This Person Does Not Exist". The number of images generated corresponds
+// to the number selected by the user. Seperate for loops are used here for the images representing people
+// who have, and have not, had a depressive episode from 2019-2020. The classes "with" or
+// "without" give the images a black or pink border, respectively.
+$(document).ready(function() {
+  $('#sample-size').on('change', function() {
+    let numPeople = Number($(this).val());
+    let str = "";
+    if (numPeople == 25) {
+      /* 2 with */
+      for (let i = 0; i < 3; i++) {
         str += `
-          <div class="col-3">
-            <img src="assets/img/users/${i}.jpg" class="img-fluid" id="person-${i}">
-          </div>
-        `;
+            <div class="col-3">
+              <img src="assets/img/users/${i}.jpg" class="img-fluid with" id="person-${i}">
+            </div>
+          `;
       }
-      $(".visualization").html(str);
-    });
+      /* 23 without */
+      for (let i = 3; i < numPeople; i++) {
+        str += `
+            <div class="col-3">
+              <img src="assets/img/users/${i}.jpg" class="img-fluid without" id="person-${i}">
+            </div>
+          `;
+      }
+    } else if (numPeople == 50) {
+      /* 4 with */
+      for (let i = 0; i < 5; i++) {
+        str += `
+            <div class="col-3">
+              <img src="assets/img/users/${i}.jpg" class="img-fluid with" id="person-${i}">
+            </div>
+          `;
+      }
+      /* 46 without */
+      for (let i = 5; i < numPeople; i++) {
+        str += `
+            <div class="col-3">
+              <img src="assets/img/users/${i}.jpg" class="img-fluid without" id="person-${i}">
+            </div>
+          `;
+      }
+    } else if (numPeople == 100) {
+      /* 8 with */
+      for (let i = 0; i < 9; i++) {
+        str += `
+            <div class="col-3">
+              <img src="assets/img/users/${i}.jpg" class="img-fluid with" id="person-${i}">
+            </div>
+          `;
+      }
+      /* 92 without */
+      for (let i = 9; i < numPeople; i++) {
+        str += `
+            <div class="col-3">
+              <img src="assets/img/users/${i}.jpg" class="img-fluid without" id="person-${i}">
+            </div>
+          `;
+      }
+    }
+    $(".visualization").html(str);
+  });
 });
-
-/**
- * Event listeners
- */
-// $(".gen").on("click", () => {
-//   // update color
-//   genOption = $("#genOption").find(":selected").text().toLowerCase();
-//   $(".outcome").html(`<ul>${randomPhrase(genOption)}</ul>`);
-// });
-// // on change
-// $("#genOption").change(() => {
-//   // trigger the click listener
-//   $(".gen").trigger("click");
-// });
-// // once page has loaded
-// $(document).ready(() => {
-//   populateSelect();
-//   $("#genOption").val(genOption);
-//   $(".gen").trigger("click");
-// });
